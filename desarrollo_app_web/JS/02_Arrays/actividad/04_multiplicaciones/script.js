68f003237469bd2d2859f71502ec5c83956dddd5
AbigailCanicura18 <abigailcaniucura@gmail.com>
2025-05-15 23:44:14 -0400

    djdn

--- desarrollo_app_web/JS/02_Arrays/actividad/04_multiplicaciones/script.js
+++ desarrollo_app_web/JS/02_Arrays/actividad/04_multiplicaciones/script.js
@@ -2,9 +2,8 @@ console.log("Conexión Establecida correctamente con js!");
 /* Crea un arreglo con los días de la semana. Luego, al hacer clic en el botón,
 muestra con alert() el día martes usando su posición en el arreglo.*/
 
-function diferenciaCentroExtremos() {
+function multiplicaPares() {
     let numeros = [4,3,2,5,1];
-    let suma = (numeros[0]+numeros[4]);
-    let resta = suma - numeros[2]
-    alert("La diferencia de los extremos de es: \n" + resta);
+    let multiplicacion = (numeros[0]*numeros[2]*numeros[4]);
+    alert("La multiplicacion de las posiciones pares tiene un valor de: \n" + multiplicacion);
 }
